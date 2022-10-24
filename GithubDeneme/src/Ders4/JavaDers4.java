baf376464b7ff50a30b0b75dc5d76ac69a56ecc5
fatih <ffaitihh.11@hotmail.com>
2022-10-24 10:22:11 +0300

    deneme

---
+++ GithubDeneme/src/Ders4/JavaDers4.java
@@ -0,0 +1,17 @@
+package Ders4;
+
+public class JavaDers4 {
+
+	public static void main(String[] args) {
+
+		int[]array = new int[10];
+		
+		System.out.printf("%s%8s%n","Index", "Value");
+		
+		for(int counter= 0 ; counter<array.length ; counter++) {
+			System.out.printf("%5d%8d%n", counter, array [counter] );
+			
+		}
+	}
+
+}
